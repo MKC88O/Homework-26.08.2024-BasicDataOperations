b72c261328bba8297df4ab232b1f336ce3854329
Максим Федоров <m88.fedorov@gmail.com>
2024-09-01 23:13:47 +0300

    Добавьте файлы проекта.

---
+++ Homework-26.08.2024-BasicDataOperations/Program.cs
@@ -0,0 +1,102 @@
+﻿using Azure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+namespace Homework_26._08._2024_BasicDataOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("appsettings.json");
+            var config = builder.Build();
+
+            string? connectionString = config.GetConnectionString("DefaultConnection");
+            
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
+            //var options = optionsBuilder.UseSqlServer("Server=DESKTOP-M496S5I;Database=testdb;Trusted_Connection=True; TrustServerCertificate=True;").Options;
+            var options = optionsBuilder.UseSqlServer(connectionString).Options;
+
+            using (ApplicationContext db = new ApplicationContext(options))
+            {
+                //    db.Database.EnsureDeleted();
+                //    db.Database.EnsureCreated();
+                Train train = new Train
+                {
+                    TrainNumber = "123A",
+                    DepartureStation = "Odessa",
+                    ArrivalStation = "Kiev",
+                    DepartureTime = DateTime.Now,
+                    ArrivalTime = DateTime.Now.AddHours(5)
+                };
+
+
+                //db.AddTrain(train);
+
+
+                //db.DeleteTrain(4);
+
+                train = db.Trains.FirstOrDefault(t => t.Id == 3);
+                if (train != null)
+                {
+                    train.TrainNumber = "1234A";
+                    db.UpdateTrain(train);
+                }
+            }
+        }
+    }
+
+    public class Train
+    {
+        public int Id { get; set; }
+        public string? TrainNumber { get; set; }
+        public string? DepartureStation { get; set; }
+        public string? ArrivalStation { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public DateTime ArrivalTime { get; set; }
+    }
+
+    class ApplicationContext : DbContext
+    {
+        public ApplicationContext() 
+        {
+        }
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Train> Trains { get; set; }
+
+        public void AddTrain(Train train)
+        {
+            Trains.Add(train);
+            SaveChanges(); 
+        }
+
+        public void DeleteTrain(int id)
+        {
+            var train = Trains.FirstOrDefault(t => t.Id == id);
+            if (train != null)
+            {
+                Trains.Remove(train);
+                SaveChanges();
+            }
+        }
+
+        public void UpdateTrain(Train newTrain)
+        {
+            Train? train = Trains.FirstOrDefault(t => t.Id == newTrain.Id);
+            if (train != null)
+            {
+                train.TrainNumber = newTrain.TrainNumber;
+                train.DepartureStation = newTrain.DepartureStation;
+                train.ArrivalStation = newTrain.ArrivalStation;
+                train.DepartureTime = newTrain.DepartureTime;
+                train.ArrivalTime = newTrain.ArrivalTime;
+                SaveChanges();
+            }
+        }
+
+    }
+}
